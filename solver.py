764c162407377ff1ee695e070538291aec23375b
Iv Robinson <aerobinsoniv@gmail.com>
2023-03-09 23:13:22 -0500

    Working solution from xxr0ss on slack

--- solver.py
+++ solver.py
@@ -8,18 +8,26 @@ EXIT1_BLOCK_ADDR = BASE_ADDR + 0x1212
 EXIT2_BLOCK_ADDR = BASE_ADDR + 0x11ec
 CONTINUE_BLOCK_ADDR = BASE_ADDR + 0x11a8
 
-def at_continue(sim: angr.sim_manager.SimulationManager):
-    if sim.active[0].addr == CONTINUE_BLOCK_ADDR:
-        return True
-    else:
-        return False
+wanted_state: SimState = None
+def ins_callback(state: SimState):
+    global wanted_state
+    if state.addr == CONTINUE_BLOCK_ADDR:
+        wanted_state = state.copy()
 
 if __name__ == "__main__":
     project = angr.Project("binary")
     entry_state = project.factory.entry_state(args=['./input.txt'])
     sim = project.factory.simgr(entry_state, veritesting=True)
 
-    # Step until we get to "Continuing"
-    sim.step(until=at_continue)
+    bp = entry_state.inspect.make_breakpoint(
+        'instruction',
+        when=angr.state_plugins.inspect.BP_BEFORE,
+        action=ins_callback
+    )
 
-    print("Ready to explore!")
+    # Step until we get to "Continuing"
+    while wanted_state is None:
+        sim.step()
+    wanted_state.inspect.remove_breakpoint('instruction', bp)
+
+    print(f"Ready to explore! State: {hex(wanted_state.addr)}")
